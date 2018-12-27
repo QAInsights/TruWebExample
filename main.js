304fed1c2a72295a425e14e9edd8b19704541388
NaveenKumar <catch.nkn@gmail.com>
2018-12-26 19:30:08 -0600

    Committing it again

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ load.initialize(async function () {
 load.action("Action", async function () {
     
     load.log(`Hello there! Let's debug`);
-    
     var a = 5;
     var b = 10;
     var sum = 0;
